b47e15bd60b5fb40476046c2c550d04d3315da27
JackHsu <hsushipei1@gmail.com>
2014-06-28 22:18:15 +0800

    0.2dev-4_modulizing the interface for user to enter backup location

---
+++ BkupLoc.py
@@ -0,0 +1,17 @@
+#!~/Software/python-stack/bin/python
+#-*- coding: utf-8 -*-
+import os, ntpath, sys
+
+"""
+ 提示使用者輸入備份存放路徑
+"""
+
+def BackupLoc():
+	print """\
+------------------------------------------------------------
+# Please enter the absolute path where you want to save the
+  backup files.
+------------------------------------------------------------"""
+	while True:
+		BkupPath = raw_input(">")
+		if os.path.exist(BkupPath)
