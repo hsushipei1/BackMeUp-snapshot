a373aa1d96d71c52d65f7fbb357ec0f3c52dfda1
JackHsu <hsushipei1@gmail.com>
2014-06-29 10:57:05 +0800

    0.2dev-11_testing: modulizing the find file part

---
+++ CreatDataBas.py
@@ -0,0 +1,12 @@
+#!~/Software/python-stack/bin/python  
+#-*- coding: utf-8 -*-
+import os, ntpath, sys
+
+"""
+建立路徑資料庫
+find ./ -name "*.c" -o -name "*.h"
+"""
+
+def FindFile(DirToBeBackup,FilExtList):
+	
+
--- main.py
+++ main.py
@@ -56,28 +56,22 @@ ProjInfo()
 AuthrInfo()
 
 ## 第一部份: 使用者輸入以及確認
-
 # 提示使用者輸入預備份的資料夾路徑
 DataBasDir = DirToBeBackup()
-
 # 提示使用者輸入預備份的副檔名類型
 FilExtList = FileExtens()
-
 # 提示使用者輸入備份存放路徑
 BkupPath = BackupLoc()
-
 # 顯示使用者剛才輸入的內容，並讓使用者選擇是否繼續還是重新輸入。
 VeryifyInput(DataBasDir,FilExtList,BkupPath)
 
+## 第二部份：利用find指令搜尋使用者指定的副檔名檔案的位置
+print "find "+DataBasDir+" -iname "+FilType+" > "+DataBaseNam+""
 sys.exit("exit from main!")
+#os.system("find "+DataBasDir+" -iname "+FilType+" > "+DataBaseNam+"")
 
-#============== 正在開發中 Under development 2014/06/20 ================
+#============== 正在開發中 Under development 2014/06/29 ================
 
-
-#-------------------
-# Establish database
-#-------------------
-os.system("find "+DataBasDir+" -iname "+FilType+" > "+DataBaseNam+"")
 
 #------------------------
 # 建立新樹狀圖、複製檔案
