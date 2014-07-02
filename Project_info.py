62fccf9dd0c07f81109ce1a828ac213a490c6f07
JackHsu <hsushipei1@gmail.com>
2014-07-02 23:56:52 +0800

    0.2init_dev-20_ 1)建立整個BackMeUp的主架構(main_flow.py)，以及各個模式選擇的module(choose_mode.py)2) 測試替文字上色的module32)下一步：讀取資料庫、複製樹狀圖

--- Project_info.py
+++ Project_info.py
@@ -3,6 +3,6 @@
 
 def ProjInfo():
 	# 專案版本資訊
-	Project = " "
+	Project = "BackMeUp"
 	Version = "1.0dev"
 	print "\n# Welcome to %s-%s" %(Project, Version)
---
+++ choose_mode.py
@@ -0,0 +1,6 @@
+#!~/Software/python-stack/bin/python  
+#-*- coding: utf-8 -*-
+
+def RightAway_Crontab_bk():
+
+
