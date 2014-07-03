507838687372617d2de45b5f144e345df9d1249c
JackHsu <hsushipei1@gmail.com>
2014-07-03 12:34:52 +0800

    0.2init_dev-22_嘗試建立一個function，可以回傳顏色的global variable(e.g. red, blue)，後面利用color_print的時候就可以使用回傳的red, blue等等

--- choose_mode.py
+++ choose_mode.py
@@ -1,12 +1,18 @@
 #!~/Software/python-stack/bin/python  
 #-*- coding: utf-8 -*-
 
-def RightAway_or_Crontab_backup():
-	print """
+from color_print import color_print
+#red =  "\33[31;1m"
+
+def immediate_or_scheduled_backup():
+	prompt1 = """
 ------------------------------------------------------------
 # There are two backup modes: 
   (1)Immediate backup =>
   (2)Configure for scheduled backup
 ------------------------------------------------------------"""
+	color_print(red, prompt1)
+
+immediate_or_scheduled_backup()
 
 
---
+++ testing/testing-print_color.py
@@ -0,0 +1,44 @@
+#!~/Software/python-stack/bin/python  
+#-*- coding: utf-8 -*-
+
+"""
+測試以module的方式把替文字上色的動作打包
+"""
+
+# 顏色(http://www.siafoo.net/snippet/88)
+default =  "\033[0m"
+red =  "\33[31;1m"
+blue = "\33[34;1m"
+gray = "\033[1;30m"
+green = "\033[1;32m"
+yellow = "\033[1;33m"
+magenta = "\033[1;35m"
+cyan = "\033[1;36m"
+white = "\033[1;37m"
+crimson = "\033[1;38m"
+
+def color_print(color,string):
+	return default =  "\033[0m"
+	return red =  "\33[31;1m"
+	return blue = "\33[34;1m"
+	return gray = "\033[1;30m"
+	return green = "\033[1;32m"
+	return yellow = "\033[1;33m"
+	return magenta = "\033[1;35m"
+	return cyan = "\033[1;36m"
+	return white = "\033[1;37m"
+	return crimson = "\033[1;38m"
+	return color+string+default
+
+print color_print(red,"HELLO")
+print color_print(blue,"HELLO")
+print color_print(gray,"HELLO")
+print color_print(green,"HELLO")
+print color_print(yellow,"HELLO")
+print color_print(magenta,"HELLO")
+print color_print(cyan,"HELLO")
+print color_print(white,"HELLO")
+print color_print(crimson,"HELLO")
+
+
+
