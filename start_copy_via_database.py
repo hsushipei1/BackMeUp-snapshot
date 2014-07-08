30913d02df8d2061437cfa6d0129c2864149aa95
JackHsu <hsushipei1@gmail.com>
2014-07-09 01:34:29 +0800

    init_dev-38: 持續開發複製的功能，目前已經完成資料夾樹狀圖的複製，接下來則是檔案的複製。

--- start_copy_via_database.py
+++ start_copy_via_database.py
@@ -2,17 +2,33 @@
 #-*- coding: utf-8 -*-
 
 from shutil import copy2
+from posixpath import *
+from os import path, makedirs
+from sys import exit
 
-def copying_keep_tree(data_base_in):
+def copying_keep_tree(data_base_in,backup_loc):
 	"""
 	Start copying, with directory tree preserved.
 	"""
+	# read data base
 	data_base = open(data_base_in)
-	
+
+	# read and handle each path(file)	
 	for per_line in data_base:
-		per_path = per_line.rstrip()
-		
+		per_path_input = per_line.rstrip()  # drop the "\n"
+
+		# backup_loc_dirs: paths of the already-backup directories
+		backup_loc_dirs = dirname(backup_loc+per_path_input)
+
+		# Copy the directory tree. "makedirs" is same as "mkdir -p"
+		if isdir(backup_loc_dirs):
+			print "exist"
+		else:
+			print "Not exist"
+			print backup_loc_dirs
+			makedirs(backup_loc_dirs)
+			
 
 
-copying_keep_tree(".sele_data_base.txt")
+copying_keep_tree(".sele_data_base.txt","/home/hsushipei/TESFDVDF")
 	
