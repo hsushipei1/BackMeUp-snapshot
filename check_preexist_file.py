d47e9096f21bd4b62b88650a62649a05e79fce84
JackHsu <hsushipei1@gmail.com>
2014-07-23 15:44:57 +0800

    init_dev-54: Start developing the "check pre-exist files"

--- check_preexist_file.py
+++ check_preexist_file.py
@@ -2,3 +2,13 @@
 #-*- coding: utf-8 -*-
 
 def check_preexist_file():
+	"""
+	If user will copy a file that is already exist in the backup location
+	BackMeUp will show the name, size, and last modified time of these 
+	files.
+	"""
+
+######### Use os.stat(file) to obtain the last modified time
+#######     and convert the time with time.strftime()
+#########     os.path.getsize(file) to get the file size
+#########
