823e9bdc7e4fb74e9d6b92e505ad1b6b0f4a0d48
JackHsu <hsushipei1@gmail.com>
2014-08-24 07:49:47 +0800

    1.0-alpha2-104: Chinese words of NT path cant show properly in NT path handler(WinXP, Win7 shows different result.)

--- Tools_for_developement/slash_searcher.sh
+++ Tools_for_developement/slash_searcher.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 
-# use grep to find any "slash" in all *.py files
-cd /work/hsushipei/BackMeUp
+#----------------
+# Slash searcher 
+#----------------
+# Use grep to find any "slash" in all *.py files
 
-for file in $(ls)
-do
-	echo ${file}
-done
+# Go to main directory
+cd ../
+
+# Search for every file
+echo "Searching 'slash'..."
+grep -R 'slash' *.py
+
+echo "Searching 'SLASH()'..."
+grep -R 'SLASH()' *.py
--- testing/handle_ntpath.py
+++ testing/handle_ntpath.py
@@ -6,30 +6,29 @@ import shutil
 import ntpath
 
 """
-目標
-1) NT路徑輸入再印出後不可以出現亂碼
-2) windows是否接受路徑中目錄以foreward slash相隔
-3) 將NT路徑輸出到螢幕上，是否可以指輸出單一正斜線
+NT Path Handler
+
+1) Input NT path(w/ backslash)
+2) Outout NT path but use foreward slash instead of backslash
+   Windows XX accepts foreward slash in file path.
 """
-# From
+# Input
 CopyFrom_prmp = "Enter the abs path to file you want to copy."
 print CopyFrom_prmp
 ToBeCopyFile_Path_In = raw_input(">")
 
-ToBeCopyFile_Path= repr(ToBeCopyFile_Path_In)[1:-1]
-ToBeCopy_FileName = ntpath.basename(ToBeCopyFile_Path)
-toBeCopy_FileParentDirName = ntpath.dirname(ToBeCopyFile_Path)
-
+# Handling
+ToBeCopyFile_Path = repr(ToBeCopyFile_Path_In)[1:-1]
 splited_path = ToBeCopyFile_Path.split("\\")
-#print splited_path
+print ToBeCopyFile_Path
+
+exit("==============")
 
 ReContru_Dirs_list = []
 
 for each_ele in splited_path:
 	if each_ele:
 		ReContru_Dirs_list.append(each_ele)
-
-#print ReContru_Dirs_list
 
 final_ntpath = "/".join(ReContru_Dirs_list)
 print final_ntpath
