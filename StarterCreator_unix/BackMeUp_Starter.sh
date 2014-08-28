f921a22d30cbe8406e5b8a8ee6d6cbdff9e76064
JackHsu <hsushipei1@gmail.com>
2014-08-28 22:26:50 +0800

    hot_FIX3_8: STARTER_CREATOR_UNIX=> Finish kernel and os release detecting section.

--- StarterCreator_unix/BackMeUp_Starter.sh
+++ StarterCreator_unix/BackMeUp_Starter.sh
@@ -1,7 +1,34 @@
 #!/bin/bash
 
-# Check and print version of python
-py_version=$(python --version)
-echo $py_version
+# check kernel(linux, BSD, or Darwin)
+# check os release
+# check python
+# check ruby
 
-# Path to main.py
+# Check user's kernel type
+kernel="unknown"
+uname_str=$(uname)
+if [[ "$uname_str" == "Linux" ]]; then
+	kernel="Linux"
+elif [[ "$uname_str" == "FreeBSD" ]]; then
+    kernel="FreeBSD"
+elif [[ "$uname_str" == "Darwin" ]]; then
+    kernel="Darwin"
+fi
+
+# Check OS release
+os_rels=$(lsb_release -d)
+
+echo $os_rels
+
+
+exit 0
+
+# Check and print version of python
+#py_version=$(pythons)
+#echo $py_version
+#if [ "$(hash python5)" = "沒有找到" ]; then
+#	echo "python沒有安裝"
+#fi
+
+## Path to main.py
