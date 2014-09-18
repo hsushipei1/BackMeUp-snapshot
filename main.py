96b3345f0f0c78bb2cdaea86b722d536dc77a897
JackHsu <hsushipei1@gmail.com>
2014-09-18 21:07:56 +0800

    1.0-alpha2-109: 1) Finish the section that creating configuration file for scheduled backup and save backup info using pickle to that file. 2) "get_userHomeDir" module is done.

--- main.py
+++ main.py
@@ -13,12 +13,10 @@ o Update Record:
 
 ##### Import modules 
 from os import system, path, getcwd
+import pickle
 from clear_console import clear_console
-# Part 0
 from project_info import ProjInfo
 from author_info import AuthrInfo
-
-# Part 1
 from backup_plan_viewer import existn_database_finder, \
 								user_input_plan_name
 from choose_mode import immediate_or_scheduled_backup,\
@@ -37,7 +35,8 @@ from verify_inputs import verify_user_inputs
 from start_copy_via_database import copying_keep_tree,\
 									copying_dont_keep_tree
 
-##### Part 0: Welcome message, project and author info
+##### IMMEDIATE BACKUP
+### Part 0: Welcome message, project and author info
 # Clear the terminal screen at startup
 clear_console()
 # Show the project and author info 
@@ -45,9 +44,9 @@ ProjInfo()
 AuthrInfo()
 # Brief intro to BackMeUp?
 
-##### Part 1: Backup configuration
+### Part 1: Backup configuration
 ## Backup Plan Viewer
-database_extension = ".BakDataBase"    # will use this var below
+database_extension = ".BakDB"    # will use this var below
 
 # Obtain the existing name of database
 (preex_database_name_list, existn_DatabaseCheck) = \
@@ -62,7 +61,18 @@ immed_or_schedu = immediate_or_scheduled_backup()
 if immed_or_schedu == "1": # immediate backup, continue
 	pass
 elif immed_or_schedu == "2": # schedule backup, jump to configure
-	configure_scheduled()
+	# Just create configuration file for scheduled backup
+	#   The content of the configuration file will be written in 
+	#   SCHEDULED BACKUP section.
+	Cret_ConfigFil4Sched = open(new_name_backup_plan+".configure","wb")
+	print "# You chose scheduled backup. "
+	print "# After all backup configuration is done, BackMeUp will run  "
+	print "   immediate backup once to deal with the pre-existing file."
+	print "   After that, BackMeUp will overwrite the existing file at each"
+	print "   scheduled backup."
+	cont = raw_input("press enter to continue.")
+	clear_console()
+	#configure_scheduled()
 
 # Path to directories that user wants to backup
 # "path_input"=>  A LIST containing paths in UNICODE type.
@@ -90,12 +100,12 @@ backup_loc = backup_location()
 backup_tag = backup_tagging()
 new_backup_loc = add_tag_2_backupLocation(backup_tag, backup_loc)
 
-##### Part 2: Verify user inputs
+### Part 2: Verify user inputs
 verify_user_inputs(new_name_backup_plan, immed_or_schedu,\
 path_input, backup_loc,	backup_style,exten_input,\
 keep_tree_value, backup_tag)
 
-##### Part 3: Start backup(copying): keep tree or not
+### Part 3: Start backup(copying): keep tree or not
 # Start copying. read data base and new backup path
 # decide to preserve directory tree or not
 if keep_tree_value == "1": # keep dir tree
@@ -103,9 +113,21 @@ if keep_tree_value == "1": # keep dir tree
 elif keep_tree_value == "2": # do not keep dir tree
     copying_dont_keep_tree(database_name, new_backup_loc)
 
-
-exit("============= FINISH COPYING ================")
-
-
-
+##### SCHEDULED BACKUP SECTION
+### Store information to backup configuration file, "Cret_ConfigFil4Sched"
+### (0) Name of backup plan
+### (1) Extension of database
+### (2) Index of keeping tree(1=keep tree, 2=dont)
+### (3) Backup location (already handled by backup tag)
+###   Store them in list in output file(using pickle)
+# Establish list to store backup configuration
+config_2save = []
+config_2save.append(new_name_backup_plan)	# Name of backup plan
+config_2save.append(database_extension)     # extension of dataB 
+config_2save.append(keep_tree_value)    # 1: keep tree, 2: Dont
+config_2save.append(backup_loc)     # Backup location(post-backupTag)
+# Store "config_2save" using pickle
+pickle.dump(config_2save, Cret_ConfigFil4Sched)
+# close file
+Cret_ConfigFil4Sched.close()
 
